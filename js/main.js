f08d1cc033757f9bf290bf4f4790beadb351d2fd
Amy Eckert <amy@amyeckertprojects.com>
2025-12-03 08:41:17 -0500

    play pause button correct now

--- js/main.js
+++ js/main.js
@@ -42,10 +42,10 @@ $(document).ready(function() {
     fade: true,
     arrows: true,
     arrowsPlacement: 'split',
-    prevArrow: '<button type="button" class="custom-prev-button">' + '  <span class="las la-angle-left" aria-hidden="true"></span>' + '  <span class="sr-only">Previous slide</span>' + '</button>',
-    nextArrow: '<button type="button" class="custom-next-button">' + '  <span class="las la-angle-right" aria-hidden="true"></span>' + '  <span class="sr-only">Next slide</span>' + '</button>',
-    pauseIcon: '<span class="la la-pause" aria-hidden="true"></span>',
-    playIcon: '<span class="la la-play" aria-hidden="true"></span>'
+    prevArrow: '<button type="button" class="custom-prev-button">' + '  <i class="fa-solid fa-angle-left"></i>' + '  <span class="sr-only">Previous slide</span>' + '</button>',
+    nextArrow: '<button type="button" class="custom-next-button">' + '  <i class="fa-solid fa-angle-right"></i>' + '  <span class="sr-only">Next slide</span>' + '</button>',
+    pauseIcon: '<i class="fa-solid fa-pause"></i>',
+    playIcon: '<i class="fa-solid fa-play"></i>'
   });
 });
 
