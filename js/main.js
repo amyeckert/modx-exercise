8d4097b9e60a7bdb96a34bc733bdd11ea782546d
Amy Eckert <amy@amyeckertprojects.com>
2025-12-05 16:11:30 -0500

    done, ugh

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     // Slick slider initialization
     $('.hero-slider').slick({
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 50000,
         slidesToShow: 1,
         dots: true,
         fade: true,
@@ -13,8 +13,8 @@ $(document).ready(function(){
         arrowsPlacement: 'split',
         prevArrow: '<button type="button" class="button button--previous">' + '  <i class="fa-solid fa-angle-left"></i>' + '  <span class="sr-only">Previous slide</span>' + '</button>',
         nextArrow: '<button type="button" class="button button--next">' + '  <i class="fa-solid fa-angle-right"></i>' + '  <span class="sr-only">Next slide</span>' + '</button>',
-        pauseIcon: '<i class="fa-solid fa-pause"><span class="sr-only">Pause</span></i>',
-        playIcon: '<i class="fa-solid fa-play"><span class="sr-only">Play</span></i>',
+        pauseIcon: '<i class="fa-solid fa-pause"></i>',
+        playIcon: '<i class="fa-solid fa-play"></i>',
         responsive: [
         {
             breakpoint: 1024,
@@ -39,42 +39,34 @@ $(document).ready(function(){
         }
         ]
     });
+});
 
-    // TOC initialization
-    window.addEventListener("load", () => {
-        // Retrieve all help sections
-        const sections = Array.from(document.querySelectorAll("h2[id], h3[id], h4[id]"));
+// TOC
+window.addEventListener("load", () => {
+    
+    const sections = Array.from(document.querySelectorAll("h2[id], h3[id]"));
 
-        console.log('sections:', sections);
-        // Once a scrolling event is detected, iterate all elements
-        // whose visibility changed and highlight their navigation entry
-        const scrollHandler = (entries) => {
-            entries.forEach(entry => {
-                const section = entry.target;
-                const sectionId = section.id;
-                const sectionLink = document.querySelector(`a[href="#${sectionId}"]`);
-                
-                console.log('section-link:', sectionLink);
+    const scrollHandler = (entries) => {
+        entries.forEach(entry => {
+            const section = entry.target;
+            const sectionId = section.id;
+            const sectionLink = document.querySelector(`a[href="#${sectionId}"]`);
 
-                if (entry.intersectionRatio > 0) {
-                    section.classList.add("visible");
-                    if (sectionLink) {
-                        sectionLink.classList.add("visible");
-                    }
-                } else {
-                    section.classList.remove("visible");
-                    if (sectionLink) {
-                        sectionLink.classList.remove("visible");
-                    }
+            if (entry.intersectionRatio > 0) {
+                section.classList.add("visible");
+                if (sectionLink) {
+                    sectionLink.classList.add("visible");
                 }
-            });
-        };
+            } else {
+                section.classList.remove("visible");
+                if (sectionLink) {
+                    sectionLink.classList.remove("visible");
+                }
+            }
+        });
+    };
 
-        // Creates a new scroll observer
-        const observer = new IntersectionObserver(scrollHandler);
+    const observer = new IntersectionObserver(scrollHandler);
 
-        //noinspection JSCheckFunctionSignatures
-        sections.forEach(section => observer.observe(section));
-    });
-
+    sections.forEach(section => observer.observe(section));
 });
